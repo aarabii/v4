fe699536343e9f213873faec915fa9eacadeb037
Gal Malach <galmalach2@gmail.com>
2022-07-19 13:58:26 +0300

    fix the navbar when scroll

--- src/components/Section/Contact/Contact.jsx
+++ src/components/Section/Contact/Contact.jsx
@@ -1,84 +1,116 @@
-import React, { useRef, useEffect, useState } from 'react';
-import emailjs from 'emailjs-com';
-import './Contact.css';
+import React, { useRef, useEffect, useState } from "react";
+import { useIsInViewport } from "../../utils/utils";
 
-import { HiOutlineMail } from 'react-icons/hi';
-import { FaTelegramPlane } from 'react-icons/fa';
-import { BsSnapchat } from 'react-icons/bs';
+import emailjs from "emailjs-com";
+import "./Contact.css";
 
+import { HiOutlineMail } from "react-icons/hi";
+import { FaTelegramPlane } from "react-icons/fa";
+import { BsSnapchat } from "react-icons/bs";
 
+const Contact = ({ setInView }) => {
+  const contactRef = useRef(null);
+  const isInViewport = useIsInViewport(contactRef);
 
-const Contact = () => {
+  useEffect(() => {
+    isInViewport && setInView("#contact");
+  }, [isInViewport]);
 
-  const Expire = props => {
+  const Expire = (props) => {
     const [visible, setVisible] = useState(true);
-  
+
     useEffect(() => {
-        const timer = setTimeout(() => {
+      const timer = setTimeout(() => {
         setVisible(false);
-        }, props.delay);
-        return () => clearTimeout(timer)
+      }, props.delay);
+      return () => clearTimeout(timer);
     }, [props.delay]);
-  
+
     return visible ? <div>{props.children}</div> : <div />;
   };
 
-  const [status, setStatus] = useState('');
+  const [status, setStatus] = useState("");
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_azymqng', 'template_jo2cvj7', form.current, 'KiorzUSNq5lRmavZ9')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_azymqng",
+        "template_jo2cvj7",
+        form.current,
+        "KiorzUSNq5lRmavZ9"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
           setStatus("Your message has been sent successfully!");
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
           setStatus("Some thing went wrong. Please try again later.");
-      });
-    e.target.reset();  
+        }
+      );
+    e.target.reset();
   };
 
   return (
-    <section id='contact'>
+    <section id="contact" ref={contactRef}>
       <h5>Say, Hello</h5>
       <h2>Contact</h2>
 
-      <div className='container contact_container'>
-        <div className='contact_options'>
-          <article className='contact_option'>
-            <HiOutlineMail className='contact_icons'/>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <HiOutlineMail className="contact_icons" />
             <h4>Email</h4>
             <h5>nishu@duck.com</h5>
-            <a href='mailto:nishu@duck.com' target="_blank" rel="noreferrer">Send a message</a>
+            <a href="mailto:nishu@duck.com" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
           </article>
 
-          <article className='contact_option'>
-            <FaTelegramPlane className='contact_icons'/>
+          <article className="contact_option">
+            <FaTelegramPlane className="contact_icons" />
             <h4>Telegram</h4>
             <h5>t.me/zzcwc</h5>
-            <a href='https://t.me/zzcwc' target="_blank" rel="noreferrer">Send a message</a>
+            <a href="https://t.me/zzcwc" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
           </article>
 
-          <article className='contact_option'>
-            <BsSnapchat className='contact_icons'/>
+          <article className="contact_option">
+            <BsSnapchat className="contact_icons" />
             <h4>Snapchat</h4>
             <h5>nishu.kr</h5>
-            <a href='https://snapchat.com/add/nishu.kr' target="_blank" rel="noreferrer">Send a message</a>
+            <a
+              href="https://snapchat.com/add/nishu.kr"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
           </article>
         </div>
         <form ref={form} onSubmit={sendEmail}>
-          <input type='text' name='name' placeholder='Your Name' required/>
-          <input type='email' name='email' placeholder='Your Email' required/>
-          <textarea name='message' rows="?" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>👋 Say Hello</button>
-          <div className='status'>
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="?"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            👋 Say Hello
+          </button>
+          <div className="status">
             <Expire delay="5000">{status}</Expire>
           </div>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Section/Experience/Exp.jsx
+++ src/components/Section/Experience/Exp.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import './Exp.css';
-import { BsPatchCheck } from 'react-icons/bs';
+import React, { useRef, useEffect } from "react";
+import { useIsInViewport } from "../../utils/utils";
 
-const Exp = () => {
+import "./Exp.css";
+import { BsPatchCheck } from "react-icons/bs";
+
+const Exp = ({ setInView }) => {
+  const expRef = useRef(null);
+  const isInViewport = useIsInViewport(expRef);
+
+  useEffect(() => {
+    isInViewport && setInView("#experience");
+  }, [isInViewport]);
+
   return (
-    <section id='experience'>
+    <section id="experience" ref={expRef}>
       <h5>Skills I have</h5>
       <h2>My Experience</h2>
 
@@ -12,111 +21,111 @@ const Exp = () => {
         <div className="experience_frontend">
           <h3>Frontend Development</h3>
           <div className="experience_content">
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>HTML5</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>CSS</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>JavaScript</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>Bootstrap</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>JQuery</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>React</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
           </div>
         </div>
 
         <div className="experience_backend">
-        <h3>Backend Development</h3>
+          <h3>Backend Development</h3>
           <div className="experience_content">
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>Node Js</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>MongoDB</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>Python</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>ExpressJs</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>Postman</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
 
-            <article className='experience_details'>
-              <BsPatchCheck className='experience_details-icons' />
+            <article className="experience_details">
+              <BsPatchCheck className="experience_details-icons" />
               <div>
                 <h4>Heroku</h4>
-                <small className='text-light'>Experienced</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
           </div>
         </div>
       </div>
-    </section>  
-  )
-}
+    </section>
+  );
+};
 
-export default Exp
+export default Exp;
