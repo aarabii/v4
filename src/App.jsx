9dad38c54bc410834143eb0edefa5ca3d391e4f3
Gal Malach <galmalach2@gmail.com>
2022-07-19 13:12:15 +0300

    add navigation to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from './components/Home/Home';
+import Home from "./components/Home/Home";
 
-import PageNotFound from './components/Pages/404Page/PageNotFound';
-import ComingSoon from './components/Pages/ComingSoon/ComingSoon';
+import PageNotFound from "./components/Pages/404Page/PageNotFound";
+import ComingSoon from "./components/Pages/ComingSoon/ComingSoon";
+import About from "./components/Section/About/About";
+import Contact from "./components/Section/Contact/Contact";
+import Exp from "./components/Section/Experience/Exp";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <Home />
           </Route>
-          <Route path='/projects'>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/experience">
+            <Exp />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/projects">
             <ComingSoon />
           </Route>
-          <Route path='/cv'>
+          <Route path="/cv">
             <ComingSoon />
           </Route>
-          <Route path='*'>
+          <Route path="*">
             <PageNotFound />
           </Route>
         </Switch>
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
