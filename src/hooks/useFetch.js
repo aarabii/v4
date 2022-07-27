135f1a81cbde8d06eb8b7069186e4df5a5ff699b
ZZCWC <85448514+losier@users.noreply.github.com>
2022-07-27 09:37:54 +0530

    ⚡added a hook and some basics formating

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,51 @@
+import { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [fetchedData, setfetchedData] = useState({
+    data: [],
+    isLoading: true,
+    error: false,
+  });
+
+  const cancelTokenSource = axios.CancelToken.source();
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axios.get(url, {
+        cancelToken: cancelTokenSource.token,
+      });
+      const data = response.data;
+
+      if (data) {
+        setfetchedData({
+          data: data.results ? data.results : data,
+          isLoading: false,
+          error: false,
+        });
+      }
+    } catch (e) {
+      if (axios.isCancel(e)) {
+        console.log("fetching data cancelled");
+      } else {
+        console.log("error:\n", e);
+      }
+
+      setfetchedData({
+        data: [],
+        isLoading: false,
+        error: true,
+      });
+    }
+  }, [url]);
+
+  useEffect(() => {
+    fetchData();
+    return () => cancelTokenSource.cancel();
+  }, [url, fetchData]);
+
+  const { data, isLoading, error } = fetchedData;
+  return { data, isLoading, error };
+};
+
+export default useFetch;
