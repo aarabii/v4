7ad58c1e9db4ca1f1697fde69fab8fed46bbcd33
ZZCWC <85448514+losier@users.noreply.github.com>
2022-09-23 12:51:31 +0530

    added notification for cv

--- src/components/Section/Nav/Nav.jsx
+++ src/components/Section/Nav/Nav.jsx
@@ -91,7 +91,25 @@ const Nav = ({ inView }) => {
           <li>
             <a
               href={"#contact"}
-              onClick={() => setActiveNav("#contact")}
+              onClick={() => {
+                Notification.requestPermission().then((perm) => {
+                  if (perm === "granted") {
+                    new Notification("Aarab's Portfolio", {
+                      body: "Seems like you are looking for my CV but unfortunately I don't have one yet. I will update this section as soon as I have one. Thank you hope you enjoy my portfolio.",
+                      icon: "https://raw.githubusercontent.com/losier/v4/master/src/assets/Logo/android-chrome-512x512.png",
+                      image:
+                        "https://raw.githubusercontent.com/losier/v4/master/src/assets/othersPics/logo.png",
+                      vibrate: true,
+                      timestamp: true,
+                      tag: "Aarab's Portfolio",
+                    });
+                  } else {
+                    alert(
+                      "Seems like we don't have access to send notifications. So i we just wanted to let you know that I don't have a CV yet. I will update this section as soon as I have one. Thank you hope you enjoy my portfolio."
+                    );
+                  }
+                });
+              }}
               className={activeNav === "#contact" ? "active" : ""}
             >
               <span>5.</span> Contact
